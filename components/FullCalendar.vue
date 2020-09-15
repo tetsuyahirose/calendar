1560b4bbcc3d0400ae336b93e6bed397a6b05b48
tetsuyahirose <tetsuya19990510gmail.com>
2020-09-15 15:50:24 +0900

    first commit

---
+++ components/FullCalendar.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <FullCalendar :options="calendarOptions">
+      <template v-slot:eventContent="arg">
+        <b>{{ arg.timeText }}</b>
+        <i>{{ arg.event.title }}</i>
+      </template>
+    </FullCalendar>
+  </div>
+</template>
+<script>
+import FullCalendar from "@fullcalendar/vue";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import jaLocale from "@fullcalendar/core/locales/ja";
+const INITIAL_EVENTS = [
+  {
+    id: 1,
+    title: "All-day event",
+    start: "2020-09-05T10:00:00",
+  },
+  {
+    id: 2,
+    title: "3day event",
+    start: "2020-09-10T10:00:00",
+    end: "2020-09-17T17:00:00",
+  },
+];
+export default {
+  components: {
+    FullCalendar,
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+        headerToolbar: {
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay",
+        },
+        initialView: "dayGridMonth",
+        initialEvents: INITIAL_EVENTS,
+        locale: jaLocale,
+        editable: true,
+        selectable: true,
+        selectMirror: true,
+        dayMaxEvents: true,
+        weekends: true,
+        select: this.handleDateSelect,
+        eventsSet: this.handleEvents,
+        eventClick: this.handleEventClick,
+        eventsSet: this.handleEvents,
+        /* you can update a remote database when these fire:
+        eventAdd:
+        eventChange:
+        eventRemove:
+        */
+      },
+    };
+  },
+  methods: {
+    handleEvents(events) {},
+    handleWeekendsToggle() {},
+    handleDateSelect(selectInfo) {},
+    handleEventClick(clickInfo) {},
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <FullCalendar />
+  </div>
+</template>
+
+<script>
+import FullCalendar from "@/components/FullCalendar";
+export default {
+  components: {
+    FullCalendar,
+  },
+};
+</script>
